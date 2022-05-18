234eae58c86b0c1fdf0303150e9057e3cfb863fd
Developer Nahid <nazrulnahid50@gmail.com>
2022-05-18 22:00:20 +0600

    upload

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-const devs = [
-    {
-        id : 1,
-        name : 'MD Nazrul Islam Nahid',
-        age : 21,
-        skill : 'MERN Stack Web Developer'
-    },
-    {
-        id : 2,
-        name : 'Mahbub',
-        age : 15,
-        skill : 'BlackChain Developer'
-    },
-    {
-        id : 3,
-        name : 'Sabbir Hosain',
-        age : 25,
-        skill : 'IOS Developer'
-    },
-    {
-        id : 4,
-        name : 'MD Nazrul Islam Nahid',
-        age : 21,
-        skill : 'MERN Stack Web Developer'
-    },
-    {
-        id : 5,
-        name : 'Asa Alam',
-        age : 27,
-        skill : 'Web Developer'
-    },
-];
 
-let data = `[{"id":1,"name":"MD Nazrul Islam Nahid","age":21,"skill":"MERN Stack Web Developer"},{"id":2,"name":"Mahbub","age":15,"skill":"BlackChain Developer"},{"id":3,"name":"Sabbir Hosain","age":25,"skill":"IOS Developer"},{"id":4,"name":"MD Nazrul Islam Nahid","age":21,"skill":"MERN Stack Web Developer"},{"id":5,"name":"Asa Alam","age":27,"skill":"Web Developer"}]`;
-console.log(devs);
-console.log(JSON.stringify(devs));
-console.log(JSON.parse(data));
+// javascript regular expression
+// ==============================
+
+
+// Create a  username pattern
+let userName  = 'raza12';
+let pattern1 = /^[a-z0-9]{6}$/ ;
+console.log(pattern1.test(userName));
+
+
+// Create an email pattern 
+let email = 'raza26@gmail.com';
+let pattern2 = /^[a-z0-9_\.]*@[a-z]{2,10}\.[a-z]{2,5}$/ ;
+console.log(pattern2.test(email));
+
+
+// Create a Bangladeshi phone number pattern
+let phoneNumber = '01835838858';
+let pattern3 = /^(\+8801|01)[0-9]{9}$/ ;
+console.log(pattern3.test(phoneNumber));
+
+
+// Create a password pattern 
+let password = 'aza24^A&^%%'
+let pattern4 = /[A-Z]{1,}[a-z0-9`~!@#$%^&**()_+]*$/ ;
+console.log(pattern4.test(password));
+
+//Create a zipcode pattern  
+let zipCode = '1234';
+let pattern5 = /^[0-9]{4}$/ ;
+console.log(pattern5.test(zipCode));
+
+
+
